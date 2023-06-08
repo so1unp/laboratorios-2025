87a2cbbb2f953df76d1873135a58b5003b243354
fep <franpaez@gmail.com>
2023-06-08 08:34:49 -0300

    Agrega maps.c

---
+++ lab-07/maps.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+
+int a = 0;
+char *p;
+
+void handler(int p)
+{
+    return;
+}
+
+int main(void) {
+    signal(SIGUSR1, handler);
+    p = (char*) malloc(sizeof(char) * 8192);
+
+    pid_t pid = fork();
+
+    // Esperamos la señal SIGUSR1
+    pause();
+
+    if (pid == 0) {
+        // Esperamos la señal SIGUSR1
+        pause();
+    } else {
+        // Esperamos la señal SIGUSR1
+        pause();
+    }
+
+    return 0;
+}
